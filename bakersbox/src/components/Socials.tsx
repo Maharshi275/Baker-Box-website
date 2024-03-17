1899fb89b63320d5451210d7d0fa65babe9a5a95
Maharshi Pandya <132074489+Maharshi275@users.noreply.github.com>
2024-03-17 18:13:01 +0530

    Update Socials.tsx

--- bakersbox/src/components/Socials.tsx
+++ bakersbox/src/components/Socials.tsx
@@ -7,24 +7,24 @@ const Socials = () => {
   return (
     <div className={styles.social}>
       <div className={styles.socialcard}
-        onClick={() => window.open('','_blank')}
+        onClick={() => window.open('https://www.instagram.com/_bakers_box?igsh=MWlvaml5bW9vMTNrZg==','_blank')}
         >
           <SocialIcon url='https://instagram.com' style={{height: 40, width: 40}}/>
-          <p>@codershub.dev</p>
+          <p>@_bakers_box</p>
         </div>
 
         <div className={styles.socialcard}
-              onClick={() => window.open('', '_blank')}
+              onClick={() => window.open('rabhatt1982@gmail.com', '_blank')}
               >
-                <SocialIcon url='https://mail.google.com' style={{height: 40, width: 40}}/>
-                <p>maharship2705@gmail.com</p>
+                <SocialIcon url='https://mail.google.com/mail/' style={{height: 40, width: 40}}/>
+                <p>rabhatt1982@gamil.com</p>
         </div>
 
         <div className={styles.socialcard}
-              onClick={() => window.open('', '_blank')}
+              onClick={() => window.open('https://www.facebook.com/profile.php?id=100063836513181&mibextid=ZbWKwL', '_blank')}
               >
-                <SocialIcon url='' style={{height: 40, width: 40}}/>
-                <p>Maharshi Pandya</p>
+                <SocialIcon url='https://www.facebook.com/' style={{height: 40, width: 40}}/>
+                <p>Baker's Box</p>
         </div>
     </div>
   )
